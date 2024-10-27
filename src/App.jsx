977a0224d44868b1d729b5bd1326b35b1cc741af
anamfirin <mohammadanam90@gmail.com>
2024-10-27 10:39:02 +0700

    upload

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-white h-[-100vh] flex justify-center items-center bg-cover">
+      <img src="" alt="" />
+    </div>
+  );
+}
+// function Button({ color, texvalue }) {
+//   return (
+//     <button className={`h-10 px-6 font-semibold rounded-md ${color} text-white`} type="submit">
+//       {" "}
+//       {texvalue}
+//     </button>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+//       <div className="flex gap-x-3">
+//         <Button color="bg-blue-700" texvalue={"login"}></Button>
+//         <Button color="bg-slate-700" texvalue={"register"}></Button>
+//         <Button></Button>
+//         <Button variant="bg-pink-700"></Button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
