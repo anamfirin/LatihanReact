629c22e7cc6c3596ac9b8618e2acef0c71ba99f9
anamfirin <mohammadanam90@gmail.com>
2024-11-01 13:45:23 +0700

    merubah project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,15 @@
-import React from "react";
+// import React from "react";
+import "./App.css";
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
-    <div className="text-white h-[-100vh] flex justify-center items-center bg-cover">
-      <img src="" alt="" />
+    <div>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
-// function Button({ color, texvalue }) {
-//   return (
-//     <button className={`h-10 px-6 font-semibold rounded-md ${color} text-white`} type="submit">
-//       {" "}
-//       {texvalue}
-//     </button>
-//   );
-// }
 
-// function App() {
-//   return (
-//     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-//       <div className="flex gap-x-3">
-//         <Button color="bg-blue-700" texvalue={"login"}></Button>
-//         <Button color="bg-slate-700" texvalue={"register"}></Button>
-//         <Button></Button>
-//         <Button variant="bg-pink-700"></Button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
